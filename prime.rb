91e3ca0963f20f430b1dc432aa5d2c32ceeb8900
Sean Febo <seanfebo@techie.com>
2018-04-06 22:31:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,21 @@
 def prime? (i)
-  if i = 1 
-    return false 
-  elsif i = 3
-    return true 
-  elsif i % 2 = 0 || i % 3 = 0 
-    return false 
-  
+   if i == 2
+        return true
+    if n == 3
+        return true
+    if n % 2 == 0
+        return false
+    if n % 3 == 0
+        return false
+
+    i = 5
+    w = 2
+
+    while i * i <= n:
+        if n % i == 0:
+            return False
+
+        i += w
+        w = 6 - w
+
+    return True
