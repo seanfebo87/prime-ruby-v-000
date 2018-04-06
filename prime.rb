c7b91738712a8bdda1dcc01db5a0e627740b8bef
Sean Febo <seanfebo@techie.com>
2018-04-06 22:41:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,6 @@
 def prime? (i)
-   if i == 2
-        return true
-    elsif i == 3
-        return true
-      elsif i <= 0 
-        return false 
-    elsif i % 2 == 0
-        return false
-    elsif i % 3 == 0
-        return false
-    end
+   if i< = 1 
+     return false 
+    elsif 
+  
 end
